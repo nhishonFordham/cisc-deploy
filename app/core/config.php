f26baa52f7af8e5dcfb703722ef3d7efae9c28b1
Nathan Hishon <nathan.hishon@gmail.com>
2024-04-27 13:25:33 -0400

    updates

--- app/core/config.php
+++ app/core/config.php
@@ -1,7 +1,24 @@
 <?php
 
-define('DBNAME', $env['DBNAME']);
-define('DBHOST', $env['DBHOST']);
-define('DBUSER', $env['DBUSER']);
-define('DBPASS', $env['DBPASS']);
-define('DBDRIVER', '');
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+    /** database config **/
+    define('DBNAME', $env['DBNAME']);
+    define('DBHOST', $env['DBHOST']);
+    define('DBUSER', $env['DBUSER']);
+    define('DBPASS', $env['DBPASS']);
+
+    define('ROOT', 'http://localhost:8888/');
+
+}else
+{
+    /** database config **/
+    define('DBNAME', getenv('DBNAME'));
+    define('DBHOST', getenv('DBHOST'));
+    define('DBUSER', getenv('DBUSER'));
+    define('DBPASS', getenv('DBPASS'));
+
+    define('ROOT', 'https://cisc3300-1-2b4be2ca4ddb.herokuapp.com/');
+
+}
+
